430b6db70318d2313110322c4616424a30f6806e
David Rodriguez Fuentes <davidrf.developer@gmail.com>
2017-11-11 17:53:53 -0500

    5-59

---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class CockpitComponent implements OnInit {
+  newServerName = '';
+  newServerContent = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onAddServer() {
+    this.serverElements.push({
+      type: 'server',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+
+  onAddBlueprint() {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+}
